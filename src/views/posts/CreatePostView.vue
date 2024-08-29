41497d2f7cf7dee711d39de6c6842598c78e18d5
ianahmfac <me.business25@gmail.com>
2024-08-29 20:14:09 +0700

    Implement create post UI

--- src/views/posts/CreatePostView.vue
+++ src/views/posts/CreatePostView.vue
@@ -36,35 +36,73 @@ async function addPost() {
 </script>
 
 <template>
-  <main>
-    <form @submit.prevent="addPost">
-      <div>
-        <label for="title">Title</label>
-        <input v-model="title" type="text" id="title" />
+  <!-- Page Header-->
+  <header class="masthead" style="background-image: url('/img/contact-bg.jpg')">
+    <div class="container position-relative px-4 px-lg-5">
+      <div class="row gx-4 gx-lg-5 justify-content-center">
+        <div class="col-md-10 col-lg-8 col-xl-7">
+          <div class="page-heading">
+            <h1>Create New Post</h1>
+          </div>
+        </div>
       </div>
-      <div>
-        <label for="body">Body</label>
-        <textarea
-          v-model="body"
-          type="text"
-          id="body"
-          cols="30"
-          rows="10"
-        ></textarea>
+    </div>
+  </header>
+  <!-- Main Content-->
+  <main class="mb-4">
+    <div class="container px-4 px-lg-5">
+      <div class="row gx-4 gx-lg-5 justify-content-center">
+        <div class="col-md-10 col-lg-8 col-xl-7">
+          <div class="my-5">
+            <form @submit.prevent="addPost">
+              <div class="form-floating">
+                <input
+                  class="form-control"
+                  id="title"
+                  type="text"
+                  placeholder="Enter title post..."
+                  v-model="title"
+                />
+                <label for="title">Title Post</label>
+              </div>
+              <div class="form-floating">
+                <textarea
+                  class="form-control"
+                  id="body"
+                  placeholder="Enter your content here..."
+                  style="height: 12rem"
+                  v-model="body"
+                ></textarea>
+                <label for="body">Content Post</label>
+              </div>
+              <div class="form-floating">
+                <input
+                  class="form-control"
+                  id="tags"
+                  type="text"
+                  placeholder="Enter title tags => ['html', 'css', 'js']"
+                  v-model="tag"
+                  @keydown.enter.prevent="insertTag"
+                />
+                <label for="tags">Tags Post</label>
+                <span class="me-1" v-for="tag in tags" :key="tag"
+                  >#{{ tag }}</span
+                >
+              </div>
+              <!-- Submit Button-->
+              <button
+                class="btn btn-primary text-uppercase"
+                id="submitButton"
+                type="submit"
+                @click.prevent="addPost"
+              >
+                Send
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-      <div>
-        <label for="tags">Tags</label>
-        <input
-          v-model="tag"
-          type="text"
-          id="tags"
-          @keydown.enter.prevent="insertTag"
-        />
-      </div>
-      <button class="btn btn-primary" type="submit">Create</button>
-    </form>
-
-    <div v-for="tag in tags" :key="tag">#{{ tag }}</div>
+    </div>
   </main>
 </template>
 
